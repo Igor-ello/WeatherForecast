dab276e7654ab144212e59147e10ec502de155b4
Igorello <orl715320@gmail.com>
2023-09-19 23:58:32 +0300

    Screen layout

--- app/src/main/java/com/obsessed/weatherforecast/screens/MainScreen.kt
+++ app/src/main/java/com/obsessed/weatherforecast/screens/MainScreen.kt
@@ -1,46 +1,121 @@
 package com.obsessed.weatherforecast.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.obsessed.weatherforecast.R
 import com.obsessed.weatherforecast.ui.theme.BlueLight
 
-@Preview (showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun MainScreen() {
-    Image(painter = painterResource(id = R.drawable.weather_bg),
+    Image(
+        painter = painterResource(id = R.drawable.weather_bg),
         contentDescription = "image",
         modifier = Modifier
             .fillMaxSize()
             .alpha(0.6f),
         contentScale = ContentScale.FillBounds
     )
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(5.dp),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp),
 
-    ) {
-        Card(modifier = Modifier
-            .fillMaxWidth(),
+        ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(),
             backgroundColor = BlueLight,
             elevation = 0.dp,
             shape = RoundedCornerShape(10.dp)
         ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "20 Jun 2022 13:51",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                        contentDescription = "image_forecast",
+                        modifier = Modifier
+                            .size(35.dp)
+                            .padding(end = 8.dp)
+                    )
+                }
+                Text(
+                    text = "Madrid",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "23°C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton( onClick = {}
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "picture_search",
+                            tint = Color.White
+                        )
+                    }
 
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "23°C/12°C",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+
+                    IconButton( onClick = {}
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "picture_sync",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
         }
     }
 }
